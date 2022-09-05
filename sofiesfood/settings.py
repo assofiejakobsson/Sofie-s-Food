2e93f6ef49ba4b875be65d50b0c704a54936f8cc
assofiejakobsson <s89j@live.se>
2022-09-05 12:26:39 +0000

    Deployment commit

--- sofiesfood/settings.py
+++ sofiesfood/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'sofiesfood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
